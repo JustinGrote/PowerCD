9e14a7064783fa0b7ff56f227b346e15bb391dca
JustinGrote <justingrote+github@gmail.com>
2018-11-27 16:11:07 -0800

    Add more AngularJS Commit Message hooks

--- build.ps1
+++ build.ps1
@@ -4,7 +4,7 @@ using namespace System.IO
 .SYNOPSIS
 Bootstraps Invoke-Build and starts it with supplied parameters.
 .NOTES
-If you already have Invoke-Build installed, just use Invoke-Build instead of this script. This is for CI/CD environments like Appveyor, Jenkins, or Azure Devops.
+If you already have Invoke-Build installed, just use Invoke-Build instead of this script. This is for CI/CD environments like Appveyor, Jenkins, or Azure DevOps pipelines.
 .EXAMPLE
 .\build.ps1
 Starts Invoke-Build with the default parameters
