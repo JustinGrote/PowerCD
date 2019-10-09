a6098f78c75683174b6b4561f85ab4a5cc4e11ce
Justin Grote <justingrote@users.noreply.github.com>
2019-10-09 10:10:43 -0700

    Use new functionality in buildhelpers

--- PowerCD/Public/Initialize-PowerCD.ps1
+++ PowerCD/Public/Initialize-PowerCD.ps1
@@ -1,4 +1,3 @@
-#requires -module BuildHelpers
 <#
 .SYNOPSIS
 Initializes the build environment and detects various aspects of the environment
@@ -10,17 +9,10 @@ function Initialize-PowerCD {
         #Specify this if you don't want initialization to switch to the folder build root
         [Switch]$SkipSetBuildRoot
     )
-    Get-BuildEnvironment
-
-
-
-    #Appveyor Detection
-    if ($ENV:APPVEYOR) {Set-Variable -Name IsAppVeyor -Scope 1 -Value $true}
-    #Azure DevOps Detection
-    if ($ENV:SYSTEM_COLLECTIONID) {Set-Variable -Name IsAzureDevOps -Scope 1 -Value $true}
+    $BuildEnvironment = BuildHelpers\Get-BuildEnvironment
 
     #Detect if we are in a continuous integration environment (Appveyor, etc.) or otherwise running noninteractively
-    if ($ENV:CI -or $CI -or $IsAppVeyor -or $IsAzureDevOps -or ([Environment]::GetCommandLineArgs() -like '-noni*')) {
+    if ($ENV:CI -or $CI -or $BuildEnvironment.BuildSystem -ne 'Unknown') {
         write-build Green 'Build Initialization - Detected a Noninteractive or CI environment, disabling prompt confirmations'
         $SCRIPT:CI = $true
         $ConfirmPreference = 'None'
