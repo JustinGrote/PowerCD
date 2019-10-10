2ef39f86fa885fdb1cb1e2bd179224392d8a3caa
Justin Grote <justingrote@users.noreply.github.com>
2019-10-09 17:52:19 -0700

    Try generic script

---
+++ PowerCD/Private/GenerateAzDevopsMatrix.ps1
@@ -0,0 +1,31 @@
+$os = @(
+    'windows-latest'
+    'vs2017-win2016'
+    'ubuntu-latest'
+    'macOS-latest'
+)
+
+$psversion = @(
+    'pwsh'
+    'powershell'
+)
+
+$exclude = 'ubuntu-latest-powershell','macOS-latest-powershell'
+
+$entries = @{}
+foreach ($osItem in $os) {
+    foreach ($psverItem in $psversion) {
+        $entries."$osItem-$psverItem" = @{os=$osItem;psversion=$psverItem}
+    }
+}
+
+$exclude.foreach{
+    $entries.Remove($PSItem)
+}
+
+$entries.keys | sort | foreach {
+    "      $PSItem`:"
+    "        os: $($entries[$PSItem].os)"
+    "        psversion: $($entries[$PSItem].psversion)"
+}
+
