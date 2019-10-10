265fb4bc25d104b5866e60a743e53119ab742323
JustinGrote <justingrote+github@gmail.com>
2019-10-10 13:48:28 -0700

    Add Build Process, Compile by Default

--- PSModule.build.psd1
+++
@@ -1,3 +0,0 @@
-@{
-    BuildOutput = "Release"
-}
--- PowerCD/Public/Build-PowerCDModule.ps1
+++ PowerCD/Public/Build-PowerCDModule.ps1
@@ -14,15 +14,21 @@ function Build-PowerCDModule {
         $Destination = $pcdSetting.BuildModuleOutput,
         #By Default this command expects a nonexistent destination, specify this to allow for a "Dirty" copy
         [Switch]$Force,
-        #If specified, will consolidate all relevant module files into a single .psm1 file. This enables the module to load faster.
-        [Switch]$Compile,
+        #By default, the build will consolidate all relevant module files into a single .psm1 file. This enables the module to load faster. Specify this if you want to instead copy the files as-is
+        [Switch]$NoCompile,
         #If you chose compile, specify this for the region block in your .psm1 file to replace with the compiled code. If not specified, it will just append to the end of the file. Defaults to 'SourceInit' for #region SourceInit
-        [String]$SourceRegionName = 'SourceInit'
+        [String]$SourceRegionName = 'SourceInit',
+        #Files that are considered for inclusion to the 'compiled' module. This by default includes .ps1 files only. Uses Filesystem Filter syntax
+        [String[]]$PSFileInclude = '*.ps1',
+        #Files that are considered for inclusion to the 'compiled' module. This excludes any files that have two periods before ps1 (e.g. .build.ps1, .tests.ps1). Uses Filesystem Filter syntax
+        [String[]]$PSFileExclude = '*.*.ps1'
     )
+
+    $SourceModuleDir = Split-Path $PSModuleManifest
 
     #Verify a clean build folder
     try {
-        $DestinationDirectory = New-Item -ItemType Directory -Path $Destination -ErrorAction Stop
+        $DestinationDirectory = New-Item -ItemType Directory -Path $Destination -ErrorAction Stop -Verbose
     } catch [IO.IOException] {
         if ($PSItem.exception.message -match 'already exists\.$') {
             throw "Folder $Destination already exists. Make sure that you cleaned your Build Output directory. To override this behavior, specify -Force"
@@ -39,10 +45,46 @@ function Build-PowerCDModule {
     #TODO: Allow .psm1 to be blank and generate it on-the-fly
     if (-not $SourceManifest.RootModule) {throw "The source manifest at $PSModuleManifest does not have a RootModule specified. This is required to build the module."}
 
-    $SourceModule = Get-Content -Raw (Join-Path (Split-Path $PSModuleManifest) $sourceManifest.RootModule)
+    $SourceRootModulePath = Join-Path $SourceModuleDir $sourceManifest.RootModule
+    $SourceRootModule = Get-Content -Raw $SourceRootModulePath
 
+    $pcdSetting.ModuleManifest = $SourceManifest
 
-    $SourceModule > (join-path $Destination $SourceManifest.RootModule)
+    #Cannot use Copy-Item Directly because the filtering isn't advanced enough (can't exclude)
+    $SourceFiles = Get-ChildItem -Path $SourceModuleDir -Include $PSFileInclude -Exclude $PSFileExclude -File -Recurse
+    if (-not $NoCompile) {
+        #TODO: Apply ordering if important (e.g. classes)
+        $CombinedSourceFiles = Get-Content -Raw $SourceFiles
+
+        #If a SourceInit region was set, inject the files there, otherwise just append to the end.
+        $sourceRegionRegex = "(?s)#region $SourceRegionName.+#endregion $SourceRegionName"
+        if ($SourceRootModule -match $sourceRegionRegex) {
+            #Need to escape the $ in the replacement string
+            $RegexEscapedCombinedSourceFiles = [String]$CombinedSourceFiles.replace('$','$$')
+            $SourceRootModule = $SourceRootModule -replace $sourceRegionRegex,$RegexEscapedCombinedSourceFiles
+        } else {
+            $SourceRootModule += [Environment]::NewLine() + $CombinedSourceFiles
+        }
+    } else {
+        #TODO: Track all files in the source directory to ensure none get missed on the second step
+        $SourceFiles | Foreach-Object {
+            #Powershell 6+ Preferred way.
+            #TODO: Enable when dropping support for building on 5.x
+            #$RelativePath = [io.path]::GetRelativePath($SourceModuleDir,$PSItem.fullname)
+
+            #Powershell 5.x compatible "Ugly" Regex method
+            $RelativePath = $PSItem.FullName -replace [Regex]::Escape($SourceModuleDir),''
+
+            #Copy-Item doesn't automatically create directory structures when copying files vs. directories
+            $DestinationPath = Join-Path $DestinationDirectory $RelativePath
+            $DestinationDir = Split-Path $DestinationPath
+            if (-not (Test-Path $DestinationDir)) {New-Item -ItemType Directory $DestinationDir -verbose > $null}
+            Copy-Item -Path $PSItem -Destination $DestinationPath -Verbose
+        }
+    }
+
+    #Output the modified Root Module
+    $SourceRootModule > (join-path $DestinationDirectory $SourceManifest.RootModule)
 
     #Copy-Module PowershellBuild
 <#
