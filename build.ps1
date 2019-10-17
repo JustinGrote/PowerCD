55a97ea0305c4e2730366eb66a0d51ddd16819cb
JustinGrote <justingrote+github@gmail.com>
2019-10-16 22:47:50 -0700

    Remove InstallModule Dependency

--- build.ps1
+++ build.ps1
@@ -44,6 +44,47 @@ Returns a path to an Invoke-Build powershell module either as a Powershell Modul
 		return $false
 	}
 }
+function Import-ModuleFast ($ModuleName,[Switch]$Package,[Switch]$Force) {
+    process {
+        #Get a temporary directory
+        $tempModulePath = [io.path]::Combine([io.path]::GetTempPath(),'PowerCD',$ModuleName)
+        $tempfile = join-path $tempModulePath "$ModuleName.zip"
+
+        if ((Test-Path $tempfile) -and -not $Force) {
+            Write-Verbose "$ModuleName already installed as $tempfile"
+            if ($Package) {$tempModulePath}
+            return
+        }
+
+        #This only happens if -Force was specified, so we require confirmation so that the user didn't do something stupid like put ../../.. in the path
+        if (Test-Path $tempModulePath) {Remove-Item $tempModulePath -Recurse -Confirm:$true}
+
+        New-Item -ItemType Directory -Path $tempModulePath > $null
+
+        #Fetch and import the module
+        $baseURI = 'https://powershellgallery.com/api/v2/package/'
+        if ($Package) {
+            $baseURI = 'https://www.nuget.org/api/v2/package/'
+        }
+        $moduleLatestURI = "$baseURI$ModuleName"
+
+        write-verbose "Fetching $ModuleName from $moduleLatestURI"
+        (New-Object Net.WebClient).DownloadFile($moduleLatestURI, $tempfile)
+
+        $CurrentProgressPreference = $ProgressPreference
+        $GLOBAL:ProgressPreference = 'silentlycontinue'
+        Expand-Archive $tempfile $tempModulePath -Force -ErrorAction stop
+        $GLOBAL:ProgressPreference = $CurrentProgressPreference
+
+        $ModuleToImportPath = Join-Path $tempModulePath "$ModuleName.psd1"
+        if (-not $Package) {
+            write-verbose "Importing $ModuleName from $ModuleToImportPath"
+            Import-Module $ModuleToImportPath -force
+        } else {
+            $tempModulePath
+        }
+    }
+}
 
 #region Main
 Write-Host -fore green "Detected Powershell $($PSVersionTable.PSEdition) $($PSVersionTable.PSVersion)"
@@ -51,9 +92,7 @@ Write-Host -fore green "Detected Powershell $($PSVersionTable.PSEdition) $($PSVe
 $InvokeBuildPath = FindInvokeBuild
 if (-not $InvokeBuildPath) {
 	#Bootstrap it
-	Import-Module PowershellGet -Verbose -Force
-	Install-Module -Name InvokeBuild -MinimumVersion $MinimumVersion -Scope CurrentUser -Force
-	Import-Module -Name InvokeBuild -MinimumVersion $MinimumVersion -Force -Scope Global
+	Import-ModuleFast InvokeBuild
 }
 
 Invoke-Expression "Invoke-Build $($args -join ' ')"
