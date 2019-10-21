3e5cb223e7376c1c2af307b8e552d3396fb04c45
Justin Grote <justingrote@users.noreply.github.com>
2019-10-21 09:57:12 -0700

    Fix

--- PSModule.build.ps1
+++ PSModule.build.ps1
@@ -17,7 +17,7 @@ if ($PSEdition -eq 'Desktop' -and ((get-module -Name 'Microsoft.PowerShell.Utili
 #Bootstrap package management in a new process. If you try to do it same-process you can't import it because the DLL from the old version is already loaded
 #YOU MUST DO THIS IN A NEW SESSION PRIOR TO RUNNING ANY PACKAGEMANGEMENT OR POWERSHELLGET COMMANDS
 #NOTES: Tried using a runspace but install-module would crap out on older PS5.x versions.
-gmo PackageManagement -ListAvailable | Out-String | Write-Warning
+Write-Warning (gmo PackageManagement -ListAvailable | select name,version | Out-String)
 
 function BootstrapPSGet {
     $psGetVersionMinimum = '2.2.1'
@@ -37,9 +37,11 @@ function BootstrapPSGet {
     Import-Module PowershellGet -Scope Global -Force -MinimumVersion 2.2 -ErrorAction Stop
 }
 BootStrapPSGet
-gmo PackageManagement -ListAvailable | Out-String | Write-Warning
+Write-Warning (gmo PackageManagement -ListAvailable | select name,version | Out-String)
 
 Import-Module PowershellGet -Scope Global -Force -MinimumVersion 2.2 -ErrorAction Stop
+
+Write-Warning (gmo PackageManagement -ListAvailable | select name,version | Out-String)
 
 #endregion Bootstrap
 Import-Module $BuildRoot\PowerCD\PowerCD -Force -WarningAction SilentlyContinue
