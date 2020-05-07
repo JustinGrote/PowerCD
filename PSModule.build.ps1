7c6efab0a2076ea34925006d79b1cb0bce962f24
Justin Grote <JustinGrote@users.noreply.github.com>
2020-05-07 14:41:35 -0700

    Feat: Assorted Tooling Updates (#28)
    * Fix: Add Error Handling to Bootstrap for existing loaded modules
    * Fix: PowerCD to txt to avoid loading
    * Fix: Make Invoke-Build Detection More Specific
    * Feat: Use Test bootstrap path
    * Docs: Add Examples for Launch Config

--- PSModule.build.ps1
+++ PSModule.build.ps1
@@ -1,8 +1,7 @@
 #requires -version 5.1
 
 #region PowerCDBootstrap
-$SCRIPT:PowerCDBootstrap = [scriptblock]::Create((iwr -useb 'https://gist.githubusercontent.com/JustinGrote/2d3fdbac302847be33de8021add524ad/raw/PowerCDBootstrap.ps1'))
-. $PowerCDBootstrap
+. ([scriptblock]::Create((Invoke-WebRequest -UseBasicParsing 'https://git.io/PCDBootstrap')))
 #endregion PowerCDBootstrap
 
 task PowerCD.Test.Pester {
