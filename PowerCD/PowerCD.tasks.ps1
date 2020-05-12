7be4b1deb7a1ccf605c37301c18c74d9ea985309
Justin Grote <jgrote@allieddigital.net>
2020-05-12 15:54:59 -0700

    Refactor: Move PowerCD-Specific Build Item Out of Script

--- PowerCD/PowerCD.tasks.ps1
+++ PowerCD/PowerCD.tasks.ps1
@@ -18,24 +18,6 @@ task PowerCD.BuildPSModule {
     Build-PowerCDModule
 }
 
-#region PowerCDSpecific
-
-#TODO: Make PowerCD-Specific task for this
-task PowerCD.CopyBuildTasks {
-    @(
-        ".config\dotnet-tools.json",
-        "PowerCD\PowerCD.bootstrap.ps1",
-        "PowerCD\PowerCD.tasks.ps1"
-    ).foreach{
-        $sourceItem = Join-Path $BuildRoot $PSItem
-        if (Test-Path $sourceItem) {
-            Copy-Item $sourceItem -Destination (Get-Item $BuildRoot\BuildOutput\PowerCD)
-        }
-    }
-}
-
-#endRegion PowerCDSpecific
-
 
 task PowerCD.UpdateVersion {
     Set-PowerCDVersion
@@ -88,7 +70,7 @@ task PowerCD.Package.Zip {
 }
 
 #region MetaTasks
-task PowerCD.Build PowerCD.Version,PowerCD.BuildPSModule,PowerCD.UpdateVersion,PowerCD.UpdatePublicFunctions,PowerCD.CopyBuildTasks
+task PowerCD.Build PowerCD.Version,PowerCD.BuildPSModule,PowerCD.UpdateVersion,PowerCD.UpdatePublicFunctions
 task PowerCD.Package PowerCD.Package.Zip,PowerCD.Package.Nuget
 task PowerCD.Test PowerCD.Test.Pester
 task PowerCD.Default PowerCD.Clean,PowerCD.Build,PowerCD.Test
